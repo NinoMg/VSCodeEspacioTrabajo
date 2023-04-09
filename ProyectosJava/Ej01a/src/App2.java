3fa7e18f656ca74d61ccb5aa17389d6e466b8af5
carlosprivitera <carlosprivitera@yahoo.com.ar>
2023-04-09 20:58:44 -0300

    m20-58

---
+++ ProyectosJava/Ej01a/src/App2.java
@@ -0,0 +1,25 @@
+
+
+public class App2 {
+
+    private int x = 8;
+    
+    public App2() {
+
+
+    }
+
+    public int optenerX() {
+
+        return x;
+
+    }
+
+    public void ponerX(int p_x) {
+     
+        x = p_x;
+
+    }
+
+
+}
