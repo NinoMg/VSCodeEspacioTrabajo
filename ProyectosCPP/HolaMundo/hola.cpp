dfe13512f8119569dfd609ebf20dcf1c026ec578
carlosprivitera <carlosprivitera@yahoo.com.ar>
2023-04-17 01:32:19 -0300

    mm

--- ProyectosCPP/HolaMundo/hola.cpp
+++ ProyectosCPP/HolaMundo/hola.cpp
@@ -4,12 +4,11 @@ using namespace std;
 
 int main(int argc, char* argv[]) {
 
-    if(argc > 1) {
       cout << "El número de argumentos es: " << argc << endl;
       for(int i = 0; i < argc; i++) {
         cout << "Argumento " << i << ": " << argv[i] << endl;
       }
-    }
+  
     cout << "Hola" << endl;
 
     return 0;
